c7f08be73594dab77ef3d7f9a382e292facc9280
komal <komal.vermacs@gmail.com>
2020-01-18 16:12:58 +0530

    some files are modified

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    # return HttpResponse('Home Page')
+    return render(request, 'index.html')
+
+
+def about(request):
+    return HttpResponse('about Page')
+
+
+def contact(request):
+    return HttpResponse('Contact Page')
+
+
+def blog(request):
+    return HttpResponse('Blog Home Page')
+
+
+def blogPost(request, slug):
+    return HttpResponse(f'Blog Post Page {slug}')
